b05ffa342f0b47447ccbb0aac3979f3d8343b919
xiihaiqal <71058792+xiihaiqal@users.noreply.github.com>
2020-10-19 17:41:34 +0800

    Update menu.sh

--- Files/Menu/menu.sh
+++ Files/Menu/menu.sh
@@ -1,9 +1,22 @@
 #!/bin/bash
-# Script by : Hiratechi
-clear
-echo -e "\e[0m                                                   "
-echo -e "\e[93m           AutoScriptVPS by  Hiratechi           "
-echo -e "\e[93m                  a.k.a reputation                "
-echo -e "\e[0m                                                   "
-echo -e "\e[93m        COMMANDS [menu/accounts/options/server]            "
-echo -e "\e[0m                                                   "
+
+echo "clear" >> .bashrc
+echo 'echo -e ""' >> .bashrc
+echo 'echo -e "Selamat datang ke server $HOSTNAME"' >> .bashrc
+echo 'echo -e ""' >> .bashrc
+echo 'echo -e "##############################################"' >> .bashrc
+echo 'echo -e "#                                            #"' >> .bashrc
+echo 'echo -e "#  Jika anda mempunyai sebarang kemusykilan  #"' >> .bashrc
+echo 'echo -e "# Boleh terus hubungi admin seperti di bawah #"' >> .bashrc
+echo 'echo -e "#                                            #"' >> .bashrc
+echo 'echo -e "#          Whatsapp - +60178760080           #"' >> .bashrc
+echo 'echo -e "#       Telegram - http://t.me/xiihaiqal     #"' >> .bashrc
+echo 'echo -e "#                                            #"' >> .bashrc
+echo 'echo -e "#          Script By KingKongVPN             #"' >> .bashrc
+echo 'echo -e "#                                            #"' >> .bashrc
+echo 'echo -e "##############################################"' >> .bashrc
+echo 'echo -e "                                              "' >> .bashrc
+echo 'echo -e "                                              "' >> .bashrc
+echo 'echo -e "        [menu/accounts/options/server]        "' >> .bashrc
+echo 'echo -e ""' >> .bashrc
+echo 'echo -e ""' >> .bashrc
